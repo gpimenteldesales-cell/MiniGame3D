786387a0ab829843b08de6c81434ede5871f43d4
GUIMê <gpimenteldesales@gmail.com>
2026-08-20 17:08:59 -0300

    CommitScriptPrototype

---
+++ Assets/Prototypes/Scripts/ScriptColorRandomize.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+//public class ScriptColorRandomize : MonoBehaviour
+//{
+//    /* 
+//     * Color.red
+//     * Color.green
+//     * Color.blue
+//     * Color.white
+//     * Color.black
+//     * Color.gray
+//     * Color.grey(Transparente)
+//     * Color.yellow
+//     * Color.cyan
+//     * Color.magenta
+//     */
+
+//    public Material[] materials;
+//    public Color = Color.green, Color.red, Color.blue, Color.purple, Color.plum, Color.blueViolet, Color.brown, Color.gray, Color.darkBlue, Color.darkOrange colors;
+//    public int random;
+//    public int randomItem;
+//    void Start()
+//    {
+//        random = Random.Range(0, 9);
+//        Instantiate(materials[random] color[random];
+//    }
+
+//    void Update()
+//    {
+        
+//    }
+//}
